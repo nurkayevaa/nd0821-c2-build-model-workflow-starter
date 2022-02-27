bc8bafeb8ee4c0c349a0ca25fb2d297b5d8a6f9d
Anel Nurkayeva <a.nurkayeva@gmail.com>
2022-02-27 15:39:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ def go(config: DictConfig):
                              "csv": "sample.csv:latest",
                              "ref": "clean_sample.csv:reference",
                              "min_price": config['etl']['min_price'],
+                             "max_price": config['etl']['max_price'],
                              "kl_threshold": config["data_check"]["kl_threshold"]
                            },
                            ) 
