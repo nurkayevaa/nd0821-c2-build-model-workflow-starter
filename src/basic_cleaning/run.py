e340890c87a3b1aa3c4e7902c8d4423f105bef43
Anel Nurkayeva <a.nurkayeva@gmail.com>
2022-02-27 23:35:57 -0800

    Update run.py

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -39,8 +39,8 @@ def go(args):
     
     # Drop outliers
     # Drop outliers
-    min_price = args.min_price
-    max_price = args.max_price
+    min_price = 10# args.min_price
+    max_price = 350#args.max_price
 
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
