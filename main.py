201b5d386a32b80f0f782e4e27b462c406a09b3e
Anel Nurkayeva <a.nurkayeva@gmail.com>
2022-02-27 19:17:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -98,7 +98,20 @@ def go(config: DictConfig):
             # Implement here #
             ##################
 
-            pass
+            _ = mlflow.run(
+                os.path.join(root_path, "random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_artifact.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": config["random_forest_export"]
+                },
+            )
+
 
         if "test_regression_model" in active_steps:
 
